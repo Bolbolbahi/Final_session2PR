bd067fb42e41700811b42b1c3c5285d9e064fed7
Bolbolbahi <93318036+Bolbolbahi@users.noreply.github.com>
2026-01-15 13:58:55 -0500

    "Création de la classe Triangle+Affichage"

---
+++ Exercise1.py
@@ -0,0 +1,20 @@
+class Triangle:
+    def __init__(self, n):
+        self.n = n
+    def triangle_gauche(self):
+        lignes=[]
+        for i in range(1,self.n+1):
+            lignes.append("*"*i)
+        return lignes
+    def triangle_droite(self):
+        lignes=[]
+        for i in range(1,self.n+1):
+            espaces=" "*(self.n-i)
+            etoiles="*"*i
+            lignes.append(espaces+etoiles)
+        return lignes
+class Affichage :
+    def afficher (self, lignes):
+        for ligne in lignes:
+            print(ligne)
+
