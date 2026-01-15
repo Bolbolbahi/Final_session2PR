2b289d8e4f07e9c781fe8bf668d47c102e5e4feb
Bolbolbahi <93318036+Bolbolbahi@users.noreply.github.com>
2026-01-15 16:02:11 -0500

    "Implémentation de la logique du calcul»"

--- Exercice2.py
+++ Exercice2.py
@@ -8,3 +8,21 @@ class CalculateurDouble(QWidget):
     def __init__(self):
         super().__init__()
         self.initialiser_interface()
+
+    def calculer_double(self):
+        try:
+            texte_valeur = self.champ_saisie.text().strip()
+
+            if not texte_valeur:
+                QMessageBox.warning(self, 'Erreur', 'Veuillez entrer une valeur.')
+                return
+            valeur = float(texte_valeur)
+            double = valeur * 2
+
+            if double == int(double):
+                self.champ_resultat.setText(str(int(double)))
+            else:
+                self.champ_resultat.setText(str(double))
+
+        except ValueError:
+            QMessageBox.warning(self, 'Erreur', 'Veuillez entrer un nombre valide.')
